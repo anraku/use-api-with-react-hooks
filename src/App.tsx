f3630cb781b1908c9ae44ecfc530efb110de0b71
anraku <anraku90@gmail.com>
2019-09-29 11:29:25 +0900

    React Hooksを使ったAPIアクセスの処理を実装

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 const App: React.FC = () => {
+  const API_URL = 'http://localhost:3000/books';
+  const [books, setBooks] = useState<Book[]>([]);
+
+  const loadData = async () => {
+    await fetch(API_URL)
+    .then(res => res.json())
+    .then(response => setBooks(response))
+    .catch(e => console.log(e));
+  }
+
+  useEffect(() => {
+    loadData();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {books.map((book, index) => 
+        <Book key={index} title={book.title} author={book.author} rating={book.rating} />
+      )}
     </div>
+  );
+}
+
+interface Book {
+  id?: number
+  title: string
+  author: string
+  rating: number
+}
+
+const Book: React.FC<Book> = props => {
+  return (
+    <>
+      <h2>{props.title}</h2>
+      <p>{props.author}</p>
+      <p>{props.rating}</p>
+    </>
   );
 }
 
